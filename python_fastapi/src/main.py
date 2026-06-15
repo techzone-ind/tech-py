61bbf52e9879562913549888ad3b1b41a958d7bd
techzone-ind <tech-beams@outlook.com>
2026-06-15 18:45:17 +0530

    python_fastapi: Added static files

--- python_fastapi/src/main.py
+++ python_fastapi/src/main.py
@@ -9,10 +9,13 @@
 from fastapi import FastAPI
 
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
 from .middleware.token_processor_middleware import TokenProcessorMiddleware
 from .routers import  users
 
 app = FastAPI(title="FastAPI demo");
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 app.add_middleware(TokenProcessorMiddleware)
 
